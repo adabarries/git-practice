af4d1d3e73ffdb4dc8a10172f4f65c58b77671f9
Joan <jakovacs@protonmail.com>
2022-03-25 13:38:52 -0700

    Updated function_a to pass the test

--- function_a.py
+++ function_a.py
@@ -1,7 +1,21 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    freq_dict = {}
+    for num in number_list:
+        if num not in freq_dict.keys():
+            freq_dict[num] = 1
+        else:
+            freq_dict[num] += 1
+
+    highest_count_num = None
+    num_count = 0
+    for num, count in freq_dict.items():
+        if count > num_count:
+            highest_count_num = num
+            num_count = count
+
+    return highest_count_num
 
 
 if __name__ == "__main__":
